d1e6360610fceadc62cf110e7524abee42f70acf
anujyadav10457 <138033429+anujyadav10457@users.noreply.github.com>
2025-03-03 16:19:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById('theme-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    let mode = document.body.classList.contains('dark-mode') ? "🌞 Light Mode" : "🌙 Dark Mode";
+    this.textContent = mode;
+});
 
